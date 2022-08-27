64a4013d0b9e8fa15ff7008ca9788af347814582
Lymanto <lymantohadibrata@gmail.com>
2022-08-27 16:06:22 +0700

    finish 4 day forecast and 3 hours forecast

--- src/App.js
+++ src/App.js
@@ -6,57 +6,104 @@ import {
   SafeAreaView,
   StyleSheet,
   ActivityIndicator,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 import {DayForecastList, HourForecastList, Header, Gap} from './components';
 
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
 const App = () => {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [isErr, setIsErr] = useState(false);
+  const [refreshing, setRefreshing] = React.useState(false);
 
+  async function getWeather() {
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=jakarta&units=metric&appid=cf7fef9a8698b4a1bb21fa55fd9a8aee`,
+      )
+      .then(res => {
+        setCurrentWeather(res);
+        setIsErr(false);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsErr(true);
+      });
+  }
+  async function getForecast() {
+    await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=jakarta&units=metric&appid=cf7fef9a8698b4a1bb21fa55fd9a8aee`,
+      )
+      .then(res => {
+        setForecast(res.data);
+        setIsErr(false);
+      })
+      .catch(err => {
+        console.log(err.data);
+        setIsErr(true);
+      });
+  }
   useEffect(() => {
-    async function getWeather() {
-      await axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=jakarta&units=metric&appid=cf7fef9a8698b4a1bb21fa55fd9a8aee`,
-        )
-        .then(res => {
-          setCurrentWeather(res);
-        })
-        .catch(err => {
-          err;
-        });
-    }
-    async function getForecast() {
-      await axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/forecast?q=jakarta&units=metric&appid=cf7fef9a8698b4a1bb21fa55fd9a8aee`,
-        )
-        .then(res => {
-          setForecast(res.data);
-        })
-        .catch(err => {
-          err.data;
-        });
-    }
     getWeather();
     getForecast();
   }, []);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => {
+      setRefreshing(false);
+      getForecast();
+      getWeather();
+    });
+  }, []);
+  if (isErr) {
+    return (
+      <SafeAreaView style={styles.loadingPage}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors="#FF4651"
+              tintColor="#FF4651"
+            />
+          }>
+          <Text>Oops...</Text>
+          <Text>Something went wrong with API</Text>
+          <Text>Please try again later</Text>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 
   if (currentWeather != null && forecast != null) {
     return (
-      <View style={styles.page}>
-        <SafeAreaView />
-        <Gap height={100} />
-        <Header
-          city={currentWeather.data.name}
-          weatherDesc={currentWeather.data.weather[0].description}
-          temp={Math.round(currentWeather.data.main.temp)}
-        />
-        <Gap height={50} />
-        <DayForecastList data={forecast} />
-        <Gap height={30} />
-        <HourForecastList data={forecast} />
-      </View>
+      <SafeAreaView style={styles.page}>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors="#FF4651"
+              tintColor="#FF4651"
+            />
+          }>
+          <Gap height={100} />
+          <Header
+            city={currentWeather.data.name}
+            weatherDesc={currentWeather.data.weather[0].description}
+            temp={Math.round(currentWeather.data.main.temp)}
+          />
+          <Gap height={50} />
+          <DayForecastList data={forecast} />
+          <Gap height={30} />
+          <HourForecastList data={forecast} />
+        </ScrollView>
+      </SafeAreaView>
     );
   } else {
     return (
